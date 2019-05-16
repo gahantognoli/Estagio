e1fa121c897e27c09f5ee555cc6b35f3083fcfee
Gabriel Antognoli <gabriel_antognoli@hotmail.com>
2019-05-16 07:54:30 -0300

    Alterações

---
+++ Estagio.Infra.Bootstrapper/SimpleInjectorMapping.cs
@@ -0,0 +1,13 @@
+﻿using GrupoAOX.Estagio.Data.Contexto;
+using SimpleInjector;
+
+namespace Estagio.Infra.Bootstrapper
+{
+    public class SimpleInjectorMapping
+    {
+        public static void Register(Container container)
+        {
+            container.Register<ContextoEstagio>(Lifestyle.Scoped);
+        }
+    }
+}
---
+++ GrupoAOX.Estagio.MVC/App_Start/SimpleInjectorInitializer.cs
@@ -0,0 +1,30 @@
+﻿[assembly: WebActivator.PostApplicationStartMethod(typeof(GrupoAOX.Estagio.MVC.App_Start.SimpleInjectorInitializer), 
+    "Initialize")]
+
+namespace GrupoAOX.Estagio.MVC.App_Start
+{
+    using global::Estagio.Infra.Bootstrapper;
+    using SimpleInjector;
+    using SimpleInjector.Integration.Web;
+    using SimpleInjector.Integration.Web.Mvc;
+    using System.Reflection;
+    using System.Web.Mvc;
+
+    public static class SimpleInjectorInitializer
+    {
+        public static void Initialize()
+        {
+            var container = new Container();
+            container.Options.DefaultLifestyle = new WebRequestLifestyle();
+            InitializeContainer(container);
+            container.RegisterMvcControllers(Assembly.GetExecutingAssembly());
+            container.Verify();
+            DependencyResolver.SetResolver(new SimpleInjectorDependencyResolver(container));
+        }
+
+        private static void InitializeContainer(Container container)
+        {
+            SimpleInjectorMapping.Register(container);
+        }
+    }
+}
